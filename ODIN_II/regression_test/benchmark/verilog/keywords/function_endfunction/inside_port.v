d9ef672ea905dfa4149a866fcc433f370269c3be
emacdo12 <emacdo12@unb.ca>
2020-07-07 09:26:05 -0300

    automatic

---
+++ ODIN_II/regression_test/benchmark/verilog/keywords/function_endfunction/inside_port.v
@@ -0,0 +1,21 @@
+module simple_op(in,out);
+
+    input  [7:0] in;
+    output [7:0] out;
+
+    assign out = flip(in);
+
+    function [7:0] flip(input [7:0] in);
+    begin
+        flip[0] = in [7];
+        flip[1] = in [6];
+        flip[2] = in [5];
+        flip[3] = in [4];
+        flip[4] = in [3];
+        flip[5] = in [2];
+        flip[6] = in [1];
+        flip[7] = in [0];
+    end
+    endfunction
+endmodule 
+	
